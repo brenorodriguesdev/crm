dfaa2711b03f88c97202e139c8b200e63fd545e7
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-24 19:24:08 -0300

    fix: retornarChamados em atendimento controller

--- presentation/controllers/retornarChamadosEmAtendimento.ts
+++ presentation/controllers/retornarChamadosEmAtendimento.ts
@@ -3,7 +3,7 @@ import { Controller } from "../contracts/controller";
 import { HttpRequest, HttpResponse } from "../contracts/http";
 import { serverError, ok } from "../contracts/httpHelper";
 
-export class EntrarController implements Controller {
+export class RetornarChamadosEmAtendimentoController implements Controller {
     constructor(private readonly retornarChamadosEmAtendimentoService: RetornarChamadosEmAtendimentoService) { }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
