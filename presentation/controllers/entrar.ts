f718f1fc3005f3e41731fec422683c8456b7897a
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-24 18:50:10 -0300

    feat: add entrar controller

---
+++ presentation/controllers/entrar.ts
@@ -0,0 +1,25 @@
+import { EntrarUseCase } from "../../domain/useCases/entrar";
+import { Validator } from "../../validation/contracts/validator";
+import { Controller } from "../contracts/controller";
+import { HttpRequest, HttpResponse } from "../contracts/http";
+import { badRequest, serverError, ok } from "../contracts/httpHelper";
+
+export class EntrarController implements Controller {
+    constructor(private readonly validator: Validator, private readonly entrarUseCase: EntrarUseCase) { }
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const error = this.validator.validate(httpRequest.body)
+            if (error) {
+                return badRequest(error)
+            }
+            const { usuario, senha } = httpRequest.body
+            const token = await this.entrarUseCase.entrar({
+                usuario,
+                senha
+            })
+            return ok(token)
+        } catch (error) {
+            return serverError()
+        }
+    }
+}
