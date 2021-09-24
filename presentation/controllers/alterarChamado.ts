5298bf929f17d0142d373b44952eb6c240e5bfb0
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-24 19:15:04 -0300

    feat: add alterarChamado controller

--- presentation/controllers/alterarChamado.ts
+++ presentation/controllers/alterarChamado.ts
@@ -1,20 +1,31 @@
+import { AlterarChamadoUseCase } from "../../domain/useCases/alterarChamado";
 import { AtenderChamadoUseCase } from "../../domain/useCases/atenderChamado";
 import { Validator } from "../../validation/contracts/validator";
 import { Controller } from "../contracts/controller";
 import { HttpRequest, HttpResponse } from "../contracts/http";
 import { badRequest, serverError, ok, created } from "../contracts/httpHelper";
 
-export class AtenderChamadoController implements Controller {
-    constructor(private readonly validator: Validator, private readonly atenderChamadoUseCase: AtenderChamadoUseCase) { }
+export class AlterarChamadoController implements Controller {
+    constructor(private readonly validator: Validator, private readonly alterarChamadoUseCase: AlterarChamadoUseCase) { }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
             const error = this.validator.validate(httpRequest.body)
             if (error) {
                 return badRequest(error)
             }
-            const { id, idChamado} = httpRequest.body
-            await this.atenderChamadoUseCase.atender({
+            const {
+                id,
                 idChamado,
+                titulo,
+                descricao,
+                idCanal,
+                idSituacao } = httpRequest.body
+            await this.alterarChamadoUseCase.alterar({
+                idChamado,
+                titulo,
+                descricao,
+                idCanal,
+                idSituacao,
                 idAtendente: id
             })
             return created()
