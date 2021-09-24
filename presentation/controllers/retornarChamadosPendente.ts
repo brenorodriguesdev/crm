cfc6d46d9ed8ec2569a9b2cca3f66c479f17c174
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-24 19:23:46 -0300

    feat: add retornarChamadosPendente controller

---
+++ presentation/controllers/retornarChamadosPendente.ts
@@ -0,0 +1,23 @@
+import { RetornarChamadosPendentesService } from "../../data/services/retornarChamadosPendentes";
+import { Validator } from "../../validation/contracts/validator";
+import { Controller } from "../contracts/controller";
+import { HttpRequest, HttpResponse } from "../contracts/http";
+import { serverError, ok, badRequest } from "../contracts/httpHelper";
+
+export class RetornarChamadosPendenteController implements Controller {
+    constructor(private readonly validator: Validator, private readonly retornarChamadosPendentesService: RetornarChamadosPendentesService) { }
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const error = this.validator.validate(httpRequest.query)
+            if (error) {
+                return badRequest(error)
+            }
+            const { idCanal } = httpRequest.query
+            const { id } = httpRequest.body
+            const chamados = await this.retornarChamadosPendentesService.retornar(idCanal, id)
+            return ok(chamados)
+        } catch (error) {
+            return serverError()
+        }
+    }
+}
