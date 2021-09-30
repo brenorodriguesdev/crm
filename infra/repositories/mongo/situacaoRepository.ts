963349bc10b8585b4ef067bba002968d8a93952e
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-30 20:21:27 -0300

    feat: implements save on situacao repository

--- infra/repositories/mongo/situacaoRepository.ts
+++ infra/repositories/mongo/situacaoRepository.ts
@@ -3,6 +3,12 @@ import { SituacaoEntity } from "../../../data/entities/situacao";
 import { MongoHelper } from "./mongoHelper";
 
 export class SituacaoRepositoryMongo implements SituacaoRepository {
+
+    async save(situacao: SituacaoEntity): Promise<void> {
+        const situacaoCollection = await MongoHelper.getCollection('situacoes')
+        await situacaoCollection.insertOne(situacao)
+    }
+
     async findById(id: number): Promise<SituacaoEntity> {
         const situacaoCollection = await MongoHelper.getCollection('situacoes')
         const situacao = await situacaoCollection.findOne({
