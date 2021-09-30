adcf1f697371b1ca19e2ecc919c6c19c0631925a
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-30 20:40:39 -0300

    feat: implements hasher

--- infra/cryptography/bcrypt/bcrypt.ts
+++ infra/cryptography/bcrypt/bcrypt.ts
@@ -1,8 +1,13 @@
 import { HashComparer } from "../../../data/contracts/hashComparer";
 import bcrypt from 'bcrypt'
+import { Hasher } from "../../../data/contracts/hasher";
 
-export class Bcrypt implements HashComparer {
+export class Bcrypt implements HashComparer, Hasher {
     async compare(value: string, hashValue): Promise<boolean> {
         return await bcrypt.compare(value, hashValue)
+    }
+
+    async hash(value: string): Promise<string> {
+        return await bcrypt.hash(value, 8)
     }
 }
