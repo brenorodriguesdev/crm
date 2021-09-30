0373baac8f3e91ecd5855c256e70218c86edfc7c
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-30 20:23:19 -0300

    feat: add criar situacao validation factory

---
+++ main/factories/validations/criarSituacao.ts
@@ -0,0 +1,12 @@
+import { CompositeValidator } from "../../../validation/composite"
+import { Validator } from "../../../validation/contracts/validator"
+import { RequiredFieldValidator } from "../../../validation/requiredField"
+
+export const makeCriarSituacaoValidation = (): Validator => {
+    const validators = []
+    const requiredFields = ['nome']
+    for (const field of requiredFields) {
+      validators.push(new RequiredFieldValidator(field))
+    }
+    return new CompositeValidator(validators)
+  }
