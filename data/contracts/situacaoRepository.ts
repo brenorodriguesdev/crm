eda4706d696009d33a953769eda2d25f0e36f52a
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-30 20:20:36 -0300

    feat: add situacaoRepository save contract

--- data/contracts/situacaoRepository.ts
+++ data/contracts/situacaoRepository.ts
@@ -1,6 +1,7 @@
 import { SituacaoEntity } from '../entities/situacao'
 
 export interface SituacaoRepository {
+    save: (situacao: SituacaoEntity) => Promise<void>
     findById: (id: number) => Promise<SituacaoEntity>
     getAll: () => Promise<SituacaoEntity[]>
 }
