47a09abce1b8e358c22c0557441c3ca3ff366a16
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-30 20:37:13 -0300

    feat: add save on atendenteRepository contract

--- data/contracts/atendenteRepository.ts
+++ data/contracts/atendenteRepository.ts
@@ -1,6 +1,7 @@
 import { AtendenteEntity } from "../entities/atendente";
 
 export interface AtendenteRepository {
+    save: (atendente: AtendenteEntity) => Promise<void>
     findByUsuario: (usuario: string) => Promise<AtendenteEntity>
     findById: (id: number) => Promise<AtendenteEntity>
 }
