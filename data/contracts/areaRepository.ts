b379e50a0a1aaf2cb337b3febf88f00d54a383f1
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-30 20:37:44 -0300

    feat:  add finById on AreaRepository contract

--- data/contracts/areaRepository.ts
+++ data/contracts/areaRepository.ts
@@ -2,4 +2,5 @@ import { AreaEntity } from '../entities/area';
 
 export interface AreaRepository {
     save: (area: AreaEntity) => Promise<void>
+    findById: (id: number) => Promise<AreaEntity>
 }
