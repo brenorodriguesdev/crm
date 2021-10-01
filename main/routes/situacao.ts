4855aec960696b4ea7b92cf1601666c27287effb
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 11:49:12 -0300

    feat: add alterarSituacao route

--- main/routes/situacao.ts
+++ main/routes/situacao.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { adaptRouter } from "../adapters/expressControllerAdapter";
 import { adaptMiddleware } from "../adapters/expressMiddlewareAdapter";
+import { makeAlterarSituacaoControler } from "../factories/controllers/alterarSituacao";
 import { makeCriarSituacaoControler } from "../factories/controllers/criarSituacao";
 import { makeDeletarSituacaoControler } from "../factories/controllers/deletarSituacao";
 import { makeAutenticarMiddleware } from "../factories/middlewares/autenticar";
@@ -9,5 +10,6 @@ import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPe
 
 export default (router: Router): void => {
     router.post('/criarSituacao', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeCriarSituacaoControler()))
+    router.put('/alterarSituacao', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeAlterarSituacaoControler()))
     router.delete('/deletarSituacao', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeDeletarSituacaoControler()))
 }
