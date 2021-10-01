a163e708327a63d9acef8326543eb3812e51168c
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 12:51:16 -0300

    feat: add retornarAtendentes route

--- main/routes/atendente.ts
+++ main/routes/atendente.ts
@@ -4,6 +4,7 @@ import { adaptMiddleware } from "../adapters/expressMiddlewareAdapter";
 import { makeAlterarAtendenteControler } from "../factories/controllers/alterarAtendente";
 import { makeCriarAtendenteControler } from "../factories/controllers/criarAtendente";
 import { makeDeletarAtendenteControler } from "../factories/controllers/deletarAtendente";
+import { makeRetornarAtendentesControler } from "../factories/controllers/retornarAtendentes";
 import { makeAutenticarMiddleware } from "../factories/middlewares/autenticar";
 import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPermissao";
 
@@ -11,5 +12,6 @@ import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPe
 export default (router: Router): void => {
     router.post('/criarAtendente', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeCriarAtendenteControler()))
     router.put('/alterarAtendente', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeAlterarAtendenteControler()))
+    router.get('/retornarAtendentes', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeRetornarAtendentesControler()))
     router.delete('/deletarAtendente', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeDeletarAtendenteControler()))
 }
