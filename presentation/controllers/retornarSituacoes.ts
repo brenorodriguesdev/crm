1987bec45a0f845839395626d85925f84858577e
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 12:38:02 -0300

    feat: add retornarSituacoes controller

---
+++ presentation/controllers/retornarSituacoes.ts
@@ -0,0 +1,17 @@
+import { RetornarCanaisUseCase } from "../../domain/useCases/retornarCanais";
+import { RetornarSituacoesUseCase } from "../../domain/useCases/retornarSituacoes";
+import { Controller } from "../contracts/controller";
+import { HttpRequest, HttpResponse } from "../contracts/http";
+import { serverError, ok } from "../contracts/httpHelper";
+
+export class RetornarSituacoesController implements Controller {
+    constructor(private readonly retornarSituacoesUseCase: RetornarSituacoesUseCase) { }
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const canais = await this.retornarSituacoesUseCase.retornar()
+            return ok(canais)
+        } catch (error) {
+            return serverError()
+        }
+    }
+}
