dad69a2b0820c8bf41c42243e0c70c03dffb7612
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 01:24:03 -0300

    feat: add deletarAtendente route

--- main/routes/atendente.ts
+++ main/routes/atendente.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { adaptRouter } from "../adapters/expressControllerAdapter";
 import { adaptMiddleware } from "../adapters/expressMiddlewareAdapter";
 import { makeCriarAtendenteControler } from "../factories/controllers/criarAtendente";
+import { makeDeletarAtendenteControler } from "../factories/controllers/deletarAtendente";
 import { makeAutenticarMiddleware } from "../factories/middlewares/autenticar";
 import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPermissao";
 
 
 export default (router: Router): void => {
     router.post('/criarAtendente', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeCriarAtendenteControler()))
+    router.delete('/deletarAtendente', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeDeletarAtendenteControler()))
 }
