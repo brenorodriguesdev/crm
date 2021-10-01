8b3c252dcd6e2ec31e9ebb4a11e0b49dbce97767
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 01:01:35 -0300

    feat: add deleteById on areaRepository contract

--- data/contracts/areaRepository.ts
+++ data/contracts/areaRepository.ts
@@ -3,4 +3,5 @@ import { AreaEntity } from '../entities/area';
 export interface AreaRepository {
     save: (area: AreaEntity) => Promise<void>
     findById: (id: number) => Promise<AreaEntity>
+    deleteById: (id: number) => Promise<void>
 }
