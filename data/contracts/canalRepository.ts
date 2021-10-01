66dea8e9650c9105d07eae6cb757d69597178481
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 00:53:27 -0300

    feat: deleteById on canalRepository contract

--- data/contracts/canalRepository.ts
+++ data/contracts/canalRepository.ts
@@ -3,5 +3,6 @@ import { CanalEntity } from '../entities/canal'
 export interface CanalRepository {
     save: (canal: CanalEntity) => Promise<void>
     findById: (id: number) => Promise<CanalEntity>
+    deleteById: (id: number) => Promise<void>
     getAll: () => Promise<CanalEntity[]>
 }
