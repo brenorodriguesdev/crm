a26bfdb2b2fcc0c65b126c5687f8df2581760864
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 11:43:24 -0300

    feat: add update on situacaoRepository contract

--- data/contracts/situacaoRepository.ts
+++ data/contracts/situacaoRepository.ts
@@ -2,6 +2,7 @@ import { SituacaoEntity } from '../entities/situacao'
 
 export interface SituacaoRepository {
     save: (situacao: SituacaoEntity) => Promise<void>
+    update: (situacao: SituacaoEntity) => Promise<void>
     findById: (id: number) => Promise<SituacaoEntity>
     deleteById: (id: number) => Promise<void>
     getAll: () => Promise<SituacaoEntity[]>
