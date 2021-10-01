9394968dad8c26f174515de4dff700ff615176af
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 00:58:48 -0300

    feat: add deletarCanal route

--- main/routes/canal.ts
+++ main/routes/canal.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { adaptRouter } from "../adapters/expressControllerAdapter";
 import { adaptMiddleware } from "../adapters/expressMiddlewareAdapter";
 import { makeCriarCanalControler } from "../factories/controllers/criarCanal";
+import { makeDeletarCanalControler } from "../factories/controllers/deletarCanal";
 import { makeAutenticarMiddleware } from "../factories/middlewares/autenticar";
 import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPermissao";
 
 
 export default (router: Router): void => {
     router.post('/criarCanal', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeCriarCanalControler()))
+    router.post('/deletarCanal', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeDeletarCanalControler()))
 }
