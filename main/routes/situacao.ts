cef2c4a927f1f4f973a6b8529b74e6fdc8536062
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 01:17:05 -0300

    feat: add deletarSituacao route

--- main/routes/situacao.ts
+++ main/routes/situacao.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { adaptRouter } from "../adapters/expressControllerAdapter";
 import { adaptMiddleware } from "../adapters/expressMiddlewareAdapter";
 import { makeCriarSituacaoControler } from "../factories/controllers/criarSituacao";
+import { makeDeletarSituacaoControler } from "../factories/controllers/deletarSituacao";
 import { makeAutenticarMiddleware } from "../factories/middlewares/autenticar";
 import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPermissao";
 
 
 export default (router: Router): void => {
     router.post('/criarSituacao', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeCriarSituacaoControler()))
+    router.delete('/deletarSituacao', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeDeletarSituacaoControler()))
 }
