0891da4e2c58214820b2e800b7c9e00ea1391f12
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 11:39:44 -0300

    feat: add alterarArea route

--- main/routes/area.ts
+++ main/routes/area.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { adaptRouter } from "../adapters/expressControllerAdapter";
 import { adaptMiddleware } from "../adapters/expressMiddlewareAdapter";
+import { makeAlterarAreaControler } from "../factories/controllers/alterarArea";
 import { makeCriarAreaControler } from "../factories/controllers/criarArea";
 import { makeDeletarAreaControler } from "../factories/controllers/deletarArea";
 import { makeAutenticarMiddleware } from "../factories/middlewares/autenticar";
@@ -9,5 +10,6 @@ import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPe
 
 export default (router: Router): void => {
     router.post('/criarArea', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeCriarAreaControler()))
+    router.put('/alterarArea', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeAlterarAreaControler()))
     router.delete('/deletarArea', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeDeletarAreaControler()))
 }
