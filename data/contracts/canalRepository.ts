1f838aa62aecaa461a1286d3550665a7db5be32c
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 11:24:15 -0300

    feat: add save on canalRepository contract

--- data/contracts/canalRepository.ts
+++ data/contracts/canalRepository.ts
@@ -2,6 +2,7 @@ import { CanalEntity } from '../entities/canal'
 
 export interface CanalRepository {
     save: (canal: CanalEntity) => Promise<void>
+    update: (canal: CanalEntity) => Promise<void>
     findById: (id: number) => Promise<CanalEntity>
     deleteById: (id: number) => Promise<void>
     getAll: () => Promise<CanalEntity[]>
