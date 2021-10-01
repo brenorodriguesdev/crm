e300f8efceaba365b0e5f5c2c4a0192a504d6317
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 12:32:03 -0300

    feat: add retornarAreas route

--- main/routes/area.ts
+++ main/routes/area.ts
@@ -4,6 +4,7 @@ import { adaptMiddleware } from "../adapters/expressMiddlewareAdapter";
 import { makeAlterarAreaControler } from "../factories/controllers/alterarArea";
 import { makeCriarAreaControler } from "../factories/controllers/criarArea";
 import { makeDeletarAreaControler } from "../factories/controllers/deletarArea";
+import { makeRetornarAreasControler } from "../factories/controllers/retornarAreas";
 import { makeAutenticarMiddleware } from "../factories/middlewares/autenticar";
 import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPermissao";
 
@@ -11,5 +12,6 @@ import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPe
 export default (router: Router): void => {
     router.post('/criarArea', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeCriarAreaControler()))
     router.put('/alterarArea', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeAlterarAreaControler()))
+    router.get('/retornarAreas', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeRetornarAreasControler()))
     router.delete('/deletarArea', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeDeletarAreaControler()))
 }
