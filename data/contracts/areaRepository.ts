dcc42e863348e818d93dc207cebae643944923c3
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 11:34:52 -0300

    feat:  add update on areaRepository contract

--- data/contracts/areaRepository.ts
+++ data/contracts/areaRepository.ts
@@ -2,6 +2,7 @@ import { AreaEntity } from '../entities/area';
 
 export interface AreaRepository {
     save: (area: AreaEntity) => Promise<void>
+    update: (area: AreaEntity) => Promise<void>
     findById: (id: number) => Promise<AreaEntity>
     deleteById: (id: number) => Promise<void>
 }
