913cf6593e4970384b5f541e445a4538aca423c7
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-10-01 12:07:00 -0300

    feat: add alterarAtendente route

--- main/routes/atendente.ts
+++ main/routes/atendente.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { adaptRouter } from "../adapters/expressControllerAdapter";
 import { adaptMiddleware } from "../adapters/expressMiddlewareAdapter";
+import { makeAlterarAtendenteControler } from "../factories/controllers/alterarAtendente";
 import { makeCriarAtendenteControler } from "../factories/controllers/criarAtendente";
 import { makeDeletarAtendenteControler } from "../factories/controllers/deletarAtendente";
 import { makeAutenticarMiddleware } from "../factories/middlewares/autenticar";
@@ -9,5 +10,6 @@ import { makeChecarPermissaoMiddleware } from "../factories/middlewares/checarPe
 
 export default (router: Router): void => {
     router.post('/criarAtendente', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeCriarAtendenteControler()))
+    router.put('/alterarAtendente', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeAlterarAtendenteControler()))
     router.delete('/deletarAtendente', adaptMiddleware(makeAutenticarMiddleware()), adaptMiddleware(makeChecarPermissaoMiddleware()), adaptRouter(makeDeletarAtendenteControler()))
 }
